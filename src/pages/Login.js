c49d7b411835661168462bf2e8b98a3832fc5641
Aditya Sharma <robinsh007@gmail.com>
2022-07-30 00:00:47 +0530

    second commit

---
+++ src/pages/Login.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useToasts } from 'react-toast-notifications';
+
+import styles from '../styles/login.module.css';
+
+import {useAuth} from '../hooks';
+import { Redirect } from 'react-router-dom';
+
+const Login = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [loggingIn,setLoggingIn] = useState(false);
+  const {addToast} = useToasts();
+  const auth = useAuth();
+  console.log(auth);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoggingIn(true);
+    if(!email || !password) {
+      return addToast('please enter both email and password',{
+        appearance:'error',
+      });
+    }
+
+    const response = await auth.login (email,password);
+    if(response.success){
+           addToast('Successfully logged in',{
+        appearance:'success'
+      });
+    } else {
+      return addToast(response.message,{
+        appearance:'error'
+      });
+    }
+    setLoggingIn(false);
+  };
+
+  if(auth.user){
+    return <Redirect to="/"/>;
+  }
+
+    return <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <span className={styles.LoginSignupHeader}>Log In</span>
+      <div className={styles.field}>
+        <input 
+        type="email" 
+        placeholder = "Email" 
+        value={email}
+        onChange = {(e)=>{setEmail(e.target.value)}}
+        />
+      </div>
+
+      <div className={styles.field}>
+      <input 
+      type="password" 
+      placeholder = "Password" 
+      value={password} 
+      onChange = {(e)=>{setPassword(e.target.value)}} 
+      />
+      </div>
+
+      <div className={styles.field} >
+        <button disabled={loggingIn}>{loggingIn ? 'Logging in...':'Log In'}</button>
+      </div>
+    </form>;
+  };
+  export default Login;
